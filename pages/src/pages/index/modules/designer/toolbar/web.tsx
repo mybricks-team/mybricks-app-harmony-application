726ddd9a8bc473682410833ced5089615b7b2963
moyukai05 <moyukai05@kuaishou.com>
2025-07-07 11:47:15 +0800

    save:修改导出应用源代码文案

--- pages/src/pages/index/modules/designer/toolbar/web.tsx
+++ pages/src/pages/index/modules/designer/toolbar/web.tsx
@@ -128,7 +128,7 @@ export const WebToolbar: React.FC<WebToolbarProps> = ({
         {/* <Toolbar.Button onClick={() => showHarmonyDownloadConfig({ onCompile, type: CompileType.harmonyModule })}>下载源码(模块)</Toolbar.Button> */}
         <Tooltip
           placement="bottom"
-          title={"下载源码"}
+          title={"导出应用源代码"}
         >
         <div className={css.export_btn} onClick={() => showHarmonyDownloadConfig({ onCompile, type: CompileType.harmonyApplication })}>
           {Export}
