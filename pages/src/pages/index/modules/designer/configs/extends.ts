5ed146b325baf0dbc7f862b6ecafa3f503a4a41f
liubiao03 <liubiao03@kuaishou.com>
2025-07-24 21:19:22 +0800

    feat: bus:getUser

--- pages/src/pages/index/modules/designer/configs/extends.ts
+++ pages/src/pages/index/modules/designer/configs/extends.ts
@@ -25,5 +25,80 @@ export default {
     //   ],
     //   outputsEditable: true,
     // },
+    {
+      id: 'bus-getUser',
+      title: '获取登录用户',
+      type: 'bus',
+      inputs: [
+        {
+          id: 'call',
+          title: '调用',
+          schema: {
+            type: 'follow',
+            description: '参数'
+          }
+        }
+      ],
+      outputs: [
+        {
+          id: 'then',
+          title: '结果',
+          schema: {
+            type: 'object',
+            description: '返回的用户信息',
+            properties: {
+              "uid": {
+                "title": "用户ID",
+                "description": "用户唯一标识",
+                "type": "string"
+              },
+              "username": {
+                "title": "用户名",
+                "description": "登录用户名",
+                "type": "string"
+              },
+              "nickname": {
+                "title": "昵称",
+                "description": "用户昵称",
+                "type": "string"
+              },
+              "phone": {
+                "title": "手机号",
+                "description": "用户绑定的手机号",
+                "type": "string"
+              },
+              "avatar": {
+                "title": "头像",
+                "description": "用户头像URL",
+                "type": "string"
+              },
+              "gender": {
+                "title": "性别",
+                "description": "用户性别：0=未知，1=男，2=女",
+                "type": "number"
+              },
+              "token": {
+                "title": "登录凭证",
+                "description": "登录凭证",
+                "type": "string"
+              },
+              "email": {
+                "title": "邮箱",
+                "description": "用户绑定的邮箱地址",
+                "type": "string"
+              }
+            }
+          }
+        },
+        {
+          id: 'catch',
+          title: '发生错误',
+          schema: {
+            type: 'string',
+            description: '错误信息'
+          }
+        }
+      ],
+    }
   ],
 };
